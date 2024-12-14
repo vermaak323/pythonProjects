ca6a0834a242f3f2fd515f2a18902e32ca681085
Aaryan Verma <97966118+vermaak323@users.noreply.github.com>
2024-12-14 11:15:51 +0530

    Add files via upload

---
+++ pongGame/border.py
@@ -0,0 +1,21 @@
+from turtle import Turtle
+
+class Border(Turtle):
+    def border(self):
+        self.hideturtle()
+        self.color("white")
+        self.forward(400)
+        self.right(90)
+        self.forward(300)
+        self.right(90)
+        self.forward(800)
+        self.right(90)
+        # self.forward(300)
+        # self.right(90)
+
+
+        
+        
+
+
+    # def dashed_line(self):
---
+++ pongGame/main.py
@@ -0,0 +1,80 @@
+from turtle import Turtle, Screen
+from paddle import Paddle
+from ball import Ball
+import time 
+from scoreboard import Scoreboard
+
+
+my_screen=Screen()
+my_screen.setup(height=600, width=800)
+my_screen.bgcolor("black") 
+my_screen.title("PONG GAME")
+my_screen.tracer(0)
+
+
+r_paddle=Paddle((350, 0))
+l_paddle=Paddle((-350, 0))
+ball=Ball()
+scoreboard=Scoreboard()
+
+
+my_screen.listen()
+my_screen.onkey(r_paddle.go_up, "Up")
+my_screen.onkey(r_paddle.go_down, "Down")
+my_screen.onkey(l_paddle.go_up, "w")
+my_screen.onkey(l_paddle.go_down, "s")
+
+
+
+
+
+game_is_on=True
+while game_is_on:
+    time.sleep(ball.move_speed)
+    my_screen.update()
+    ball.move()
+
+
+    #detect collision with upper and lower walls 
+    if ball.ycor()>250 or ball.ycor()<-250:
+        ball.y_bounce()
+
+    #detect collision with paddle
+    if ball.distance(r_paddle)<50 and ball.xcor()>320:
+        ball.x_bounce()
+        scoreboard.r_point()
+
+    if ball.distance(l_paddle)<50 and ball.xcor()<-320:
+        ball.x_bounce()
+        scoreboard.l_point()
+    
+        
+
+    #when r paddle misses the ball
+    if ball.xcor()>380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    # when l paddle misses the ball
+    if ball.xcor()<-380:
+        ball.reset_position()
+        scoreboard.r_point()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+my_screen.exitonclick()
